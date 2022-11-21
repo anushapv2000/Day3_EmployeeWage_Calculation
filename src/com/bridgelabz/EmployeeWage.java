0040af1b61b3b8cee79d985453a9d9cb888c94e4
anushapv2000 <anusha.pv@btech.christuniversity.in>
2022-11-21 20:29:58 +0530

    Employee wages based on condition of total hours

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -9,20 +9,24 @@ public static void main(String[] args)
     int workingdaysmonth=20;
     int d_count=1;
     int t_wages=0;
+    int totalworkinghours=100;
+    int t_hours=0;
     System.out.println("Welcome to Employee Wage Computation");
 
    // System.out.println(emp_check);
-    while (d_count<=workingdaysmonth) {
+    while (d_count<=workingdaysmonth && t_hours<=totalworkinghours) {
         int emp_check = (int) (Math.random() * 10) % 3;
         switch (emp_check) {
             case 1:
                 System.out.println("employee is fulltime");
                 dailywage = Wageperhour * Fulldayhour;
+                t_hours+=Fulldayhour;
                 System.out.println("fulltime wage="+dailywage);
                 break;
             case 2:
                 dailywage = Parttimehour * Wageperhour;
                 System.out.println("Employee is parttime");
+                t_hours+=Parttimehour;
                 System.out.println("parttime wage="+dailywage);
                 break;
             default:
@@ -33,5 +37,6 @@ public static void main(String[] args)
         d_count=d_count+1;
     }
     System.out.println("employee wage for 1 month="+t_wages);
+    System.out.println("Total working hours="+t_hours);
 }
 }
