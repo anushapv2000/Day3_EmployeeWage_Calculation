1521b5890cf3c7abb20329f239a770bf8f3d75ce
anushapv2000 <anusha.pv@btech.christuniversity.in>
2022-11-21 19:51:13 +0530

    Employee dailywage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -2,12 +2,16 @@ package com.bridgelabz;
 
 public class EmployeeWage {
 public static void main(String[] args)
-{
+{   int Wageperhour=20;
+    int Fulldayhour=8;
+    int dailywage=0;
     System.out.println("Welcome to Employee Wage Computation");
     int emp_check=(int)(Math.random()*10)%2;
    // System.out.println(emp_check);
     if(emp_check==1){
         System.out.println("employee present");
+        dailywage=Wageperhour*Fulldayhour;
+        System.out.println("Daily wage = "+dailywage);
     }
     else {
         System.out.println("employee absent");
