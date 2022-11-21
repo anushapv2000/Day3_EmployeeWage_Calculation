c8a3afca22273f13f24d08d42d2ddbf0e2406954
anushapv2000 <anusha.pv@btech.christuniversity.in>
2022-11-21 20:07:18 +0530

    Employee wages switch case

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -9,17 +9,19 @@ public static void main(String[] args)
     System.out.println("Welcome to Employee Wage Computation");
     int emp_check=(int)(Math.random()*10)%3;
    // System.out.println(emp_check);
-    if(emp_check==1){
-        System.out.println("employee is fulltime");
-        dailywage=Wageperhour*Fulldayhour;
-        //System.out.println("fulltime wage = "+dailywage);
-    } else if (emp_check==2) {
-        dailywage=Parttimehour*Wageperhour;
-        System.out.println("Employee is parttime");
-        //System.out.println("Partime Wage="+dailywage);
-    } else {
-        System.out.println("employee absent");
+    switch (emp_check){
+        case 1:
+            System.out.println("employee is fulltime");
+            dailywage=Wageperhour*Fulldayhour;
+            break;
+        case 2:
+            dailywage=Parttimehour*Wageperhour;
+            System.out.println("Employee is parttime");
+            break;
+        default:
+            System.out.println("employee absent");
     }
+
     System.out.println("employee wage="+dailywage);
 }
 }
